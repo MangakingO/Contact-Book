05ae782f44f79dd17baefc50487066f052c607f9
Oscar Barrios Jimenez <barriososcar001@yahoo.com>
2023-07-06 20:55:51 -0500

    first commit

---
+++ src/index.js
@@ -0,0 +1,451 @@
+// DO NOT EDIT THIS ARRAY //
+////////////////////////////
+/*
+ The contacts array contain the list of contacts for the contact book.
+*/
+window.contacts = [
+  {
+    id: 1,
+    name: "Leanne Graham",
+    username: "Bret",
+    email: "Sincere@april.biz",
+    picture: "https://via.placeholder.com/150/008600/FFFFFF?text=LG",
+    address: {
+      street: "Kulas Light",
+      suite: "Apt. 556",
+      city: "Green Vale",
+      zipcode: "92998-3874",
+      geo: {
+        lat: "-37.3159",
+        lng: "81.1496",
+      },
+    },
+    phone: "1-770-736-8031 x56442",
+    website: "hildegard.org",
+    company: {
+      name: "Romaguera-Crona",
+      catchPhrase: "Multi-layered client-server neural-net",
+      bs: "harness real-time e-markets",
+    },
+  },
+  {
+    id: 2,
+    name: "Ervin Howell",
+    email: "Shanna@melissa.tv",
+    picture: "https://via.placeholder.com/150/0000FF/FFFFFF?text=EH",
+    address: {
+      street: "Victor Plains",
+      suite: "Suite 879",
+      city: "Blue Vale",
+      zipcode: "90566-7771",
+      geo: {
+        lat: "-43.9509",
+        lng: "-34.4618",
+      },
+    },
+    phone: "010-692-6593 x09125",
+    website: "anastasia.net",
+    company: {
+      name: "Deckow-Crist",
+      catchPhrase: "Proactive didactic contingency",
+      bs: "synergize scalable supply-chains",
+    },
+  },
+  {
+    id: 3,
+    name: "Clementine Bauch",
+    email: "Nathan@yesenia.net",
+    picture: "https://via.placeholder.com/150/34F500/FFFFFF?text=CB",
+    address: {
+      street: "Douglas Extension",
+      suite: "Suite 847",
+      city: "Green Vale",
+      zipcode: "59590-4157",
+      geo: {
+        lat: "-68.6102",
+        lng: "-47.0653",
+      },
+    },
+    phone: "1-463-123-4447",
+    website: "ramiro.info",
+    company: {
+      name: "Romaguera-Jacobson",
+      catchPhrase: "Face to face bifurcated interface",
+      bs: "e-enable strategic applications",
+    },
+  },
+  {
+    id: 4,
+    name: "Patricia Lebsack",
+    email: "Julianne.OConner@kory.org",
+    picture: "https://via.placeholder.com/150/008600/FFFFFF?text=PL",
+    address: {
+      street: "Hoeger Mall",
+      suite: "Apt. 692",
+      city: "South Vale",
+      zipcode: "53919-4257",
+      geo: {
+        lat: "29.4572",
+        lng: "-164.2990",
+      },
+    },
+    phone: "493-170-9623 x156",
+    website: "kale.biz",
+    company: {
+      name: "Robel-Corkery",
+      catchPhrase: "Multi-tiered zero tolerance productivity",
+      bs: "transition cutting-edge web services",
+    },
+  },
+  {
+    id: 5,
+    name: "Chelsey Dietrich",
+    email: "Lucio_Hettinger@annie.ca",
+    picture: "https://via.placeholder.com/150/FF00FF/FFFFFF?text=CD",
+    address: {
+      street: "Skiles Walks",
+      suite: "Suite 351",
+      city: "North Vale",
+      zipcode: "33263",
+      geo: {
+        lat: "-31.8129",
+        lng: "62.5342",
+      },
+    },
+    phone: "(254)954-1289",
+    website: "demarco.info",
+    company: {
+      name: "Keebler LLC",
+      catchPhrase: "User-centric fault-tolerant solution",
+      bs: "revolutionize end-to-end systems",
+    },
+  },
+  {
+    id: 6,
+    name: "Mrs. Dennis Schulist",
+    email: "Karley_Dach@jasper.info",
+    picture: "https://via.placeholder.com/150/000086/FFFFFF?text=DS",
+    address: {
+      street: "Norberto Crossing",
+      suite: "Apt. 950",
+      city: "South Vale",
+      zipcode: "23505-1337",
+      geo: {
+        lat: "-71.4197",
+        lng: "71.7478",
+      },
+    },
+    phone: "1-477-935-8478 x6430",
+    website: "ola.org",
+    company: {
+      name: "Considine-Lockman",
+      catchPhrase: "Synchronised bottom-line interface",
+      bs: "e-enable innovative applications",
+    },
+  },
+  {
+    id: 7,
+    name: "Kurtis Weissnat",
+    email: "Telly.Hoeger@billy.biz",
+    picture: "https://via.placeholder.com/150/860000/FFFFFF?text=KW",
+    address: {
+      street: "Rex Trail",
+      suite: "Suite 280",
+      city: "Blue Vale",
+      zipcode: "58804-1099",
+      geo: {
+        lat: "24.8918",
+        lng: "21.8984",
+      },
+    },
+    phone: "210.067.6132",
+    website: "elvis.io",
+    company: {
+      name: "Johns Group",
+      catchPhrase: "Configurable multimedia task-force",
+      bs: "generate enterprise e-tailers",
+    },
+  },
+  {
+    id: 8,
+    name: "Nicholas Runolfsdottir V",
+    email: "Sherwood@rosamond.me",
+    picture: "https://via.placeholder.com/150/FF0000/FFFFFF?text=NR",
+    address: {
+      street: "Ellsworth Summit",
+      suite: "Suite 729",
+      city: "Green Vale",
+      zipcode: "45169",
+      geo: {
+        lat: "-14.3990",
+        lng: "-120.7677",
+      },
+    },
+    phone: "586.493.6943 x140",
+    website: "jacynthe.com",
+    company: {
+      name: "Abernathy Group",
+      catchPhrase: "Implemented secondary concept",
+      bs: "e-enable extensible e-tailers",
+    },
+  },
+  {
+    id: 9,
+    name: "Glenna Reichert",
+    email: "Chaim_McDermott@dana.io",
+    picture: "https://via.placeholder.com/150/226688/FFFFFF?text=GR",
+    address: {
+      street: "Dayna Park",
+      suite: "Suite 449",
+      city: "White Vale",
+      zipcode: "76495-3109",
+      geo: {
+        lat: "24.6463",
+        lng: "-168.8889",
+      },
+    },
+    phone: "(775)976-6794 x41206",
+    website: "conrad.com",
+    company: {
+      name: "Yost and Sons",
+      catchPhrase: "Switchable contextually-based project",
+      bs: "aggregate real-time technologies",
+    },
+  },
+  {
+    id: 10,
+    name: "Clementina DuBuque",
+    email: "Rey.Padberg@karina.biz",
+    picture: "https://via.placeholder.com/150/000000/FFFFFF?text=CD",
+    address: {
+      street: "Kattie Turnpike",
+      suite: "Suite 198",
+      city: "Blue Vale",
+      zipcode: "31428-2261",
+      geo: {
+        lat: "-38.2386",
+        lng: "57.2232",
+      },
+    },
+    phone: "024-648-3804",
+    website: "ambrose.net",
+    company: {
+      name: "Hoeger LLC",
+      catchPhrase: "Centralized empowering task-force",
+      bs: "target end-to-end models",
+    },
+  },
+];
+
+///////////////////////////////////
+// WRITE YOUR SOLUTION CODE HERE //
+///////////////////////////////////
+
+function renderContact(contact) {
+  const card = document.createElement("div");
+  card.classList.add("card");
+  card.dataset.id = contact.id;
+
+
+  const deleteButton = document.createElement("button");
+  deleteButton.classList.add("deleteBtn");
+  deleteButton.title = "Delete this contact";
+  deleteButton.innerText = "X";
+  deleteButton.addEventListener("click", () => {
+    deleteContact(contact.id);
+    render(contacts);
+  });
+
+  const avatar = document.createElement("div");
+  avatar.classList.add("avatar");
+
+  const circle1 = document.createElement("div");
+  circle1.classList.add("circle");
+  avatar.appendChild(circle1);
+
+  const circle2 = document.createElement("div");
+  circle2.classList.add("circle");
+  avatar.appendChild(circle2);
+
+  const img = document.createElement("img");
+  img.src = contact.picture;
+  avatar.appendChild(img);
+
+  const info = document.createElement("div");
+  info.classList.add("info");
+
+  const name = document.createElement("span");
+  name.classList.add("name", "big");
+  name.innerText = contact.name;
+  info.appendChild(name);
+
+  const email = document.createElement("span");
+  email.classList.add("email", "small");
+  email.innerText = contact.email;
+  info.appendChild(email);
+
+  const details = document.createElement("div");
+  details.classList.add("details");
+
+  const phone = document.createElement("div");
+  phone.classList.add("phone");
+  phone.innerText = contact.phone;
+  details.appendChild(phone);
+
+  const website = document.createElement("div");
+  website.classList.add("website");
+  website.innerText = contact.website;
+  details.appendChild(website);
+
+  const additional = document.createElement("div");
+  additional.classList.add("additional");
+
+  const address = document.createElement("div");
+  address.classList.add("address");
+
+  const suite = document.createElement("div");
+  suite.classList.add("suite");
+  suite.innerText = contact.address.suite;
+  address.appendChild(suite);
+
+  const street = document.createElement("div");
+  street.classList.add("street");
+  street.innerText = contact.address.street;
+  address.appendChild(street);
+
+  const city = document.createElement("div");
+  city.classList.add("city");
+  city.innerText = `${contact.address.city}, ${contact.address.zipcode}`;
+  address.appendChild(city);
+
+  additional.appendChild(address);
+
+  const company = document.createElement("div");
+  company.classList.add("company");
+
+  const label = document.createElement("div");
+  label.classList.add("label");
+  label.innerText = "Works at";
+  company.appendChild(label);
+
+  const companyName = document.createElement("div");
+  companyName.classList.add("company-name");
+  companyName.innerText = contact.company.name;
+  company.appendChild(companyName);
+
+  additional.appendChild(company);
+
+  card.appendChild(deleteButton);
+  card.appendChild(avatar);
+  card.appendChild(info);
+  card.appendChild(details);
+  card.appendChild(additional);
+
+  return card;
+}
+
+function loadCities(contacts) {
+  const select = document.getElementById("filterOptions");
+  select.innerHTML = "";
+
+  const uniqueCities = new Set();
+  for (const contact of contacts) {
+    uniqueCities.add(contact.address.city);
+  }
+
+  const defaultOption = document.createElement("option");
+  defaultOption.value = "0";
+  defaultOption.innerText = "-- Select a city --";
+  select.appendChild(defaultOption);
+
+  for (const city of uniqueCities) {
+    const option = document.createElement("option");
+    option.value = city;
+    option.innerText = city;
+    select.appendChild(option);
+  }
+}
+
+function render(contacts) {
+  const contactSection = document.getElementById("contacts");
+  contactSection.innerHTML = "";
+
+  if (!contacts || contacts.length === 0) {
+    return;
+  }
+
+  for (const contact of contacts) {
+    const contactCard = renderContact(contact);
+    contactSection.appendChild(contactCard);
+  }
+
+  loadCities(contacts);
+}
+
+
+function filterByCity(city) {
+  const filteredContacts = contacts.filter(
+    (contact) => contact.address.city === city
+  );
+  return filteredContacts;
+}
+
+function filterHandler() {
+  const select = document.getElementById("filterOptions");
+  const selectedCity = select.value;
+
+  if (selectedCity === "0") {
+    render(contacts);
+  } else {
+    const filteredContacts = filterByCity(selectedCity);
+    render(filteredContacts);
+  }
+}
+
+function deleteContact(id) {
+  const index = contacts.findIndex((contact) => contact.id === id);
+  if (index !== -1) {
+    contacts.splice(index, 1);
+  }
+}
+
+function deleteButtonHandler() {
+  const deleteButtons = document.querySelectorAll(".deleteBtn");
+  deleteButtons.forEach((button) => {
+    button.addEventListener("click", (event) => {
+      const card = event.target.closest(".card");
+      const id = parseInt(card.dataset.id);
+      const updatedContacts = contacts.filter((contact) => contact.id !== id);
+      contacts = updatedContacts;
+      render(contacts);
+    });
+  });
+}
+
+
+function main() {
+  const select = document.getElementById("filterOptions");
+  select.addEventListener("change", filterHandler);
+
+  deleteButtonHandler();
+
+  render(contacts);
+}
+
+main();
+
+
+
+
+//////////////////////////////////////////
+// DO NOT EDIT ANYTHING BELOW THIS LINE //
+//////////////////////////////////////////
+window.contacts = contacts;
+window.renderContact = renderContact;
+window.render = render;
+window.filterByCity = filterByCity;
+window.filterHandler = filterHandler;
+window.loadCities = loadCities;
+window.deleteContact = deleteContact;
+window.deleteButtonHandler = deleteButtonHandler;
+window.main = main;
